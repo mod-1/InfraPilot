fb0ef5a482d8fe21581a746bbd7d82653c6d4a65
Sarasa P <50651864+sarasap@users.noreply.github.com>
2024-12-13 16:51:01 -0500

    Update rds.tf

--- infrastructure_api/static/terraform_templates/rds.tf
+++ infrastructure_api/static/terraform_templates/rds.tf
@@ -8,7 +8,7 @@ module "rds_template_{unique_id}" {
 
 output "rds_template_output_{unique_id}" {
   description = "The endpoint of the RDS instance"
-  value       = module.rds_template_{unique_id}.rds_endpoint
+  value       = "${module.rds_template_{unique_id}.rds_endpoint},admin,password123"
 }
 output "rds_username_{unique_id}" {
   description = "The RDS database username"
